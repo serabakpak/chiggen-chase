34670ac034129ca967aa6a231c6a551d54b5563a
Sera Bak <serabak15@gmail.com>
2016-08-28 13:11:51 -0700

    adds winner check and audio sounds

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    var $playerOne = $('#player-one');
-    var $playerTwo = $('#player-two');
+    var $homerSimpson = $('#homer');
+    var $peterGriffin = $('#peter');
     
     $('#go').click(function() {
         var chickenWidth = $('#chicken').width();
@@ -8,29 +8,45 @@ $(document).ready(function() {
         $('#go').removeClass('infinite');
         $('#chicken').removeClass('rollIn');
         $('#chicken').addClass('infinite bounce');
-
-        $('#chicken').animate({
-            left: trackWidth
-        }, 7000);
-    });
-    
-
-
-    $(document).keydown(function(key) {
-        //console.log(key.which);
+        $('#chicken').animate({left: trackWidth}, 7000);
         
-        var positionOne = $($playerOne).position();
-        var positionTwo = $($playerTwo).position();
-        switch(key.which) {
-            
-            case 39: //move player one right
-                $playerOne.css('left', positionOne.left + 20 + 'px');
-                break;
-            case 88: //move player two right
-                $playerTwo.css('left', positionTwo.left + 20 + 'px');
-                break;
-        }
+        $(document).keydown(function(key) {
+            //console.log(key.which);
+            var positionOne = $($homerSimpson).position();
+            var positionTwo = $($peterGriffin).position();
+            console.log(positionOne.left + $($homerSimpson).width() + 20);
+            if (positionOne.left + $($homerSimpson).width() >= trackWidth) {
+                $(document).off('keydown');
+                $('#container').append('<img id="dinner" src="imgs/winnerwinnerchickendinner.png"></img>');
+                $('h1').text('Homer Simpson Wins!!!!');
+                var audio = new Audio('media/burp.wav');
+                audio.play();
+                return;
+            }
+            if (positionTwo.left + $($peterGriffin).width() >= trackWidth) {
+                $(document).off('keydown');
+                $('#container').append('<img id="dinner" src="imgs/winnerwinnerchickendinner.png"></img>');
+                $('h1').text('Peter Griffin Wins!!!!');
+                var audio = new Audio('media/laugh.mp3');
+                audio.play();
+                
+                return;
+            }
+            switch(key.which) {
+                
+                case 91: 
+                    $homerSimpson.css('left', positionOne.left + 40 + 'px');
+                    break;
+                case 39: 
+                    $peterGriffin.css('left', positionTwo.left + 40 + 'px');
+                    break;
+            }
+
+        });
+
+
 
     });
-
+      
 });
+
