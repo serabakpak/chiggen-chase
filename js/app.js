8df14c9f4d46057b8b47f92603e3ca84f8870dbb
Sera Bak <serabak15@gmail.com>
2016-08-28 00:32:01 -0700

    changes player animation to position increment and adds chicken animation to js/app.js

--- js/app.js
+++ js/app.js
@@ -1,28 +1,36 @@
 $(document).ready(function() {
+    var $playerOne = $('#player-one');
+    var $playerTwo = $('#player-two');
     
-    $(document).keydown(function(key) {
-        switch(parseInt(key.which, 10)) {
-            // Left arrow key pressed
-            case 37:
-                $('#player-one').animate({left: "-=50px"}, 'fast');
-                break;
-        
-            // Right Arrow Pressed
-            case 39:
-                $('#player-one').animate({left: "+=50px"}, 'fast');
-                break;
-        }     
-        switch(parseInt(key.which, 10)) {
-             // Left arrow key pressed
-            case 90:
-                $('#player-two').animate({left: "-=50px"}, 'fast');
-                break;
-        
-            // Right Arrow Pressed
-            case 88:
-                $('#player-two').animate({left: "+=50px"}, 'fast');
-                break;   
-            
-        }
+    $('#go').click(function() {
+        var chickenWidth = $('#chicken').width();
+        var trackWidth = $(document).width() - chickenWidth;
+        $('#go').removeClass('infinite');
+        $('#chicken').removeClass('rollIn');
+        $('#chicken').addClass('infinite bounce');
+
+        $('#chicken').animate({
+            left: trackWidth
+        }, 7000);
     });
+    
+
+
+    $(document).keydown(function(key) {
+        //console.log(key.which);
+        
+        var positionOne = $($playerOne).position();
+        var positionTwo = $($playerTwo).position();
+        switch(key.which) {
+            
+            case 39: //move player one right
+                $playerOne.css('left', positionOne.left + 20 + 'px');
+                break;
+            case 88: //move player two right
+                $playerTwo.css('left', positionTwo.left + 20 + 'px');
+                break;
+        }
+
+    });
+
 });
