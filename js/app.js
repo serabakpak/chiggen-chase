f2aa49f75ecbb34fa74ec0e235fd96ea654f6205
Sera Bak <serabak15@gmail.com>
2016-08-28 19:27:02 -0700

    adds extra stylings, player instructions and comments

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
     var $homerSimpson = $('#homer');
     var $peterGriffin = $('#peter');
+    
     var audioChicken = new Audio('media/Angry-chicken.mp3');
     audioChicken.addEventListener('ended', function() {
     this.currentTime = 0;
     this.play();
     }, false);
     audioChicken.play();
+    
+    //toggle button turns chicken sound on/off
     $('#audio').click(function() {
         if (audioChicken.paused == false) {
             audioChicken.pause();
@@ -16,13 +19,13 @@ $(document).ready(function() {
         });
 
     
-    //click the Catch It button to start the game
+    //click the 'catch it' button to start the game
     $('#go').click(function() {
         var chickenWidth = $('#chicken').width();
         var trackWidth = $(document).width() - chickenWidth;
         
         $('#go').removeClass('infinite');
-        $('#chicken').removeClass('rollIn');
+        $('img').removeClass('rollIn');
         $('#chicken').addClass('infinite bounce');
         $('#chicken').animate({left: trackWidth}, 4000);
         
@@ -31,7 +34,7 @@ $(document).ready(function() {
             var positionOne = $($homerSimpson).position();
             var positionTwo = $($peterGriffin).position();
             
-            
+            //check for winner
             if (positionOne.left + $($homerSimpson).width() >= trackWidth) {
                 $(document).off('keydown');
                 $('#container').append('<img id="dinner" src="imgs/winnerwinnerchickendinner.png"></img>');
@@ -41,6 +44,7 @@ $(document).ready(function() {
                 $('#reset').addClass('animated infinite pulse');
                 return;
             }
+
             if (positionTwo.left + $($peterGriffin).width() >= trackWidth) {
                 $(document).off('keydown');
                 $('#container').append('<img id="dinner" src="imgs/winnerwinnerchickendinner.png"></img>');
@@ -48,12 +52,10 @@ $(document).ready(function() {
                 var audio = new Audio('media/laugh.mp3');
                 audio.play();
                 $('#reset').addClass('animated infinite pulse');
-                
                 return;
             }
             
             switch(key.which) {
-                
                 case 90: // press z to make homer go
                     $homerSimpson.css('left', positionOne.left + 40 + 'px');
                     break;
@@ -61,13 +63,11 @@ $(document).ready(function() {
                     $peterGriffin.css('left', positionTwo.left + 40 + 'px');
                     break;
             }
-
         });
 
+        //press reset button to reset the game
         $('#reset').click(reset);
-
-    });
-      
+    });     
 });
 
 function reset() {
